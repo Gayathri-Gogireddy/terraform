2c9004fa64b4e2677d9cb971d05daed225e50f60
Gayathri-Gogireddy <143862109+Gayathri-Gogireddy@users.noreply.github.com>
2024-07-26 17:54:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "example" {
 
 resource "azurerm_storage_account" "example" {
   name                     = "hellodemo"
-  resource_group_name      = azurerm_resource_group.example.demo_name
+  resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
